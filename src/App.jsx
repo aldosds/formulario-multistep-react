5529355750316a638a122213d233b6ce1f3e7c80
Aldo Santos <aldoacesso@gmail.com>
2026-03-19 11:34:58 -0300

    Main component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>Formulário Multistep com React</h1>
-    </>
+    <div className="app">
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>
+          Ficamos felizes com a sua compra, utilize o formulário abaixo para
+          avaliar o produto
+        </p>
+      </div>
+      <div className="form-container">
+        <p>etapas</p>
+        <form>
+          <div className="inputs-container"></div>
+          <div className="actions">
+            <button type="button">
+              <GrFormPrevious />
+              <span>voltar</span>
+            </button>
+            <button type="submit">
+              <span>Avançar</span>
+              <GrFormNext />
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
 
