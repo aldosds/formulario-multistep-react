9729e677ddc455845a7896c4cb966c5224190302
Aldo Santos <aldoacesso@gmail.com>
2026-03-20 14:47:56 -0300

    The Thanks component for submitting the evaluation has been prepared

--- src/components/Thanks/Thanks.jsx
+++ src/components/Thanks/Thanks.jsx
@@ -1,8 +1,29 @@
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+} from "react-icons/bs";
+
+import "./Thanks.css";
+
 const Thanks = () => {
   return (
-    <>
-      <h2>Thanks</h2>
-    </>
+    <div className="thanks-container">
+      <h2>Falta pouco...</h2>
+      <p>
+        A sua opinião é muito importante, em breve você receberá um cupom de 10%
+        de desconto para sua próxima compra.
+      </p>
+      <p>Para concluir sua avaliação, clique no botão de Enviar abaixo.</p>
+      <h3>Aqui está o resultado da sua avaliação:</h3>
+      <p className="review-data">
+        <span>Santisfação com o produto:</span>
+      </p>
+      <p className="review-data">
+        <span>Comentário:</span>
+      </p>
+    </div>
   );
 };
 
