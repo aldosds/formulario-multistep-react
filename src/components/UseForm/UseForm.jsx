b900737f4b3fb3ba2e7a761c51ebab5d5c20621a
Aldo Santos <aldoacesso@gmail.com>
2026-03-20 10:17:06 -0300

    Created form for UseForm

--- src/components/UseForm/UseForm.jsx
+++ src/components/UseForm/UseForm.jsx
@@ -1,7 +1,26 @@
 const UserForm = () => {
   return (
     <>
-      <h2>User Form</h2>
+      <div className="form-control">
+        <label htmlFor="name">nome:</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Digite o seu nome"
+          required
+        />
+      </div>
+      <div className="form-control">
+        <label htmlFor="email">E-mail:</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Digite o seu e-mail"
+          required
+        />
+      </div>
     </>
   );
 };
