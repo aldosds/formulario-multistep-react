9fbd5eec62a456286908b3fe7d020075cbc3a590
Aldo Santos <aldoacesso@gmail.com>
2026-03-23 15:37:30 -0300

    Data persistence across stages was initiated in ReviewForm.jsx

--- src/components/ReviewForm/ReviewForm.jsx
+++ src/components/ReviewForm/ReviewForm.jsx
@@ -7,27 +7,55 @@ import {
   BsFillEmojiFrownFill,
 } from "react-icons/bs";
 
-const ReviewForm = () => {
+const ReviewForm = ({ data, updateFieldHandler }) => {
   return (
     <div className="review-form">
       <div className="form-control score-container">
         <label className="radio-container">
-          <input type="radio" value="unsatisfied" name="review" required />
+          <input
+            type="radio"
+            value="unsatisfied"
+            name="review"
+            required
+            checked={data.review === "unsatisfied"}
+            onChange={(e) => updateFieldHandler("review", e.target.value)}
+          />
           <BsFillEmojiFrownFill />
           <p>Insatisfeito</p>
         </label>
         <label className="radio-container">
-          <input type="radio" value="neutral" name="review" required />
+          <input
+            type="radio"
+            value="neutral"
+            name="review"
+            required
+            checked={data.review === "neutral"}
+            onChange={(e) => updateFieldHandler("review", e.target.value)}
+          />
           <BsFillEmojiNeutralFill />
           <p>poderia ser melhor</p>
         </label>
         <label className="radio-container">
-          <input type="radio" value="satisfied" name="review" required />
+          <input
+            type="radio"
+            value="satisfied"
+            name="review"
+            required
+            checked={data.review === "satisfied"}
+            onChange={(e) => updateFieldHandler("review", e.target.value)}
+          />
           <BsFillEmojiSmileFill />
           <p>Satisfeito</p>
         </label>
         <label className="radio-container">
-          <input type="radio" value="very_satisfied" name="review" required />
+          <input
+            type="radio"
+            value="very_satisfied"
+            name="review"
+            required
+            checked={data.review === "very_satisfied"}
+            onChange={(e) => updateFieldHandler("review", e.target.value)}
+          />
           <BsFillEmojiHeartEyesFill />
           <p>Muito satisfeito</p>
         </label>
@@ -39,6 +67,8 @@ const ReviewForm = () => {
           id="comment"
           placeholder="Conte como foi a sua experiência com o produto..."
           required
+          value={data.comment || ""}
+          onChange={(e) => updateFieldHandler("comment", e.target.value)}
         ></textarea>
       </div>
     </div>
